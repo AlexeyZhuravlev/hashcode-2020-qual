ae29ee5c103f7c15de1b7641da29a63affde2257
Miron Levkov <mlevkov@yandex-team.ru>
2020-02-20 23:01:25 +0300

    miron v2

--- src/miron.cpp
+++ src/miron.cpp
@@ -13,26 +13,43 @@
 #include <map>
 #include <cstdlib>
 #include <chrono>
+#include <unordered_set>
 
 using namespace std;
 
 struct TQueueEntry {
     int LibNum;
-    long Score;
+    double Score;
+    vector<int> LibraryPrefixSum;
+    vector<int> Books;
 };
 
 struct MySolver : public Context {
     void Solve() {
-        vector<vector<int>> LibraryPrefixSum(L);
         auto daysLeft = D;
+        unordered_set<int> usedBookIds;
 
-        auto recalcScore = [&](const TQueueEntry& entry) {
+        auto updatePrefixSum = [&](TQueueEntry& entry) {
+            entry.LibraryPrefixSum = {0};
+            vector<int> filteredBooks;
+            for (auto i = 0; i < entry.Books.size(); ++i) {
+                auto book = entry.Books[i];
+                if (usedBookIds.find(book) != usedBookIds.end()) {
+                    continue;
+                }
+                filteredBooks.push_back(book);
+                entry.LibraryPrefixSum.push_back(entry.LibraryPrefixSum.back() + Scores[book]);
+            }
+            entry.Books = std::move(filteredBooks);
+        };
+
+        auto calcScore = [&](const TQueueEntry& entry) -> double {
             const auto& lib = Libs[entry.LibNum];
             if (daysLeft <= lib.T) {
                 return 0;
             }
-            long long lastBook = min(1ll * lib.N, 1ll * lib.M * (daysLeft - lib.T));
-            return LibraryPrefixSum[entry.LibNum][lastBook];
+            long long lastBook = min(1llu * entry.Books.size(), 1llu * lib.M * (daysLeft - lib.T));
+            return static_cast<double>(entry.LibraryPrefixSum[lastBook]) / static_cast<double>(lib.T);
         };
 
         auto taskComparator = [&] (const auto& lhs, const auto& rhs) {
@@ -44,52 +61,40 @@ struct MySolver : public Context {
             taskQueue(taskComparator);
 
         for (auto i = 0; i < L; ++i) {
-            const auto& lib = Libs[i];
-            vector<int> curLibPrefixSum(lib.N + 1);
-            curLibPrefixSum[0] = 0;
-            auto numBatches = (lib.N + lib.M - 1) / lib.M;
-            for (auto j = 0; j < numBatches; ++j) {
-                for (auto k = j * lib.M; k < min((j + 1) * lib.M, lib.N); ++k) {
-                    curLibPrefixSum[k + 1] = curLibPrefixSum[k] + Scores[lib.Books[k]];
-                }
-            }
-            LibraryPrefixSum[i] = std::move(curLibPrefixSum);
             TQueueEntry newEntry{
                 .LibNum = i,
-                .Score = 0
+                .Score = 0.,
+                .LibraryPrefixSum = {},
+                .Books = Libs[i].Books
             };
-            newEntry.Score = recalcScore(newEntry);
+            // calc prefix sum for whole library
+            updatePrefixSum(newEntry);
+            newEntry.Score = calcScore(newEntry);
             taskQueue.push(std::move(newEntry));
         }
 
         while (daysLeft > 0 && !taskQueue.empty()) {
             auto curEntry = taskQueue.top();
             taskQueue.pop();
-            auto newScore = recalcScore(curEntry);
+
+            if (curEntry.Score == 0) {
+                continue;
+            }
+
+            updatePrefixSum(curEntry);
+            auto newScore = calcScore(curEntry);
             if (newScore != curEntry.Score) {
                 curEntry.Score = newScore;
                 taskQueue.push(curEntry);
                 continue;
             }
-            if (curEntry.Score == 0) {
-                continue;
-            }
+
             daysLeft -= Libs[curEntry.LibNum].T;
             Solution.SignedLibs.push_back(TSolLib{
                 .Id = curEntry.LibNum,
-                .Books = {}
+                .Books = curEntry.Books
             });
-            const auto& lib = Libs[curEntry.LibNum];
-            for (auto i = 0; i < daysLeft; ++i) {
-                long long firstBook = i * lib.M;
-                long long lastBook = min(lib.N, (i + 1) * lib.M);
-                for (auto j = firstBook; j < lastBook; ++j) {
-                    Solution.SignedLibs.back().Books.push_back(lib.Books[j]);
-                }
-                if (lastBook == lib.N) {
-                    break;
-                }
-            }
+            usedBookIds.insert(curEntry.Books.begin(), curEntry.Books.end());
         }
     }
 };
